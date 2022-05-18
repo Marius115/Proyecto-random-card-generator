6a81bdd05a18d8894b2174ffc5c3f5ec5c8567f4
Marius115 <cascaranomeister@gmail.com>
2022-05-18 16:51:12 +0000

    :sparkles: Listo el card generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,41 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let generaterandomNumber = () => {
+    let number = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumber = Math.floor(Math.random() * number.length);
+    return number[indexNumber];
+  };
+  let randomPinta = generateRandomPinta();
+  document.querySelector(".top-card").textContent = randomPinta;
+  document.querySelector(".bot-card").textContent = randomPinta;
+  document.querySelector(".bot-card").classList.add("boca-arriba");
+
+  document.querySelector(".number").innerHTML = generaterandomNumber();
+
+  function generateRandomPinta() {
+    let Pinta = ["♦", "♥", "♠", "♣"];
+    let indexPinta = Math.floor(Math.random() * Pinta.length);
+    return Pinta[indexPinta];
+  }
+
+  if (randomPinta == "♦" || randomPinta == "♥") {
+    document.querySelector(".top-card").style.color = "red";
+    document.querySelector(".bot-card").style.color = "red";
+    document.querySelector(".number").style.color = "red";
+  }
 };
